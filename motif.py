591b1aa87f4cfa8feb8957ee8d69a6317b58317a
Terry Chan <terrychan999@gmail.com>
2023-01-12 15:40:19 +0800

    update motif.py

--- motif.py
+++ motif.py
@@ -29,7 +29,7 @@ def genMotif(motif_length, fasta):
 
 def diff(seq1, seq2):
     score = 0
-    for index in range(len(seq1)):
+    for index in range(motif_length):
         if seq1[index] != seq2[index]:
             score += 1
     return score
@@ -37,6 +37,7 @@ def diff(seq1, seq2):
 
 def genScoreTable(motifDict):
     fasta_id1, motif1 = list(motifDict.items())[0]
+    mutated_motif1 = list()
     score_table = dict()
     for seq1 in motif1:
         for mutation in ["A", "T", "C", "G"]:
@@ -45,14 +46,17 @@ def genScoreTable(motifDict):
                 if mutation != seq1[character]:
                     mutated[character] = mutation
                     mutated_str = "".join(mutated)
-                    for fasta_id, motif in list(motifDict.items())[1:]:
-                        for seq in motif:
-                            score = diff(seq, mutated_str)
-                            if score <= 1:
-                                if mutated_str not in score_table:
-                                    score_table[mutated_str] = 1
-                                else:
-                                    score_table[mutated_str] += 1
+                    mutated_motif1.append(mutated_str)
+
+    for mutated_str in mutated_motif1:
+        for fasta_id, motif in list(motifDict.items())[1:]:
+            for seq in motif:
+                score = diff(seq, mutated_str)
+                if score <= 1:
+                    if mutated_str not in score_table:
+                        score_table[mutated_str] = 1
+                    else:
+                        score_table[mutated_str] += 1
     return score_table
 
 
@@ -63,4 +67,9 @@ if __name__ == "__main__":
     score_table = genScoreTable(motifDict)
     score_table = sorted(score_table.items(), key=lambda x: x[1])
     k, v = score_table[-1]
-    print(k)
+    print(f"possible motif: {k}")
+    for fasta_id, motif in motifDict.items():
+        for index, seq in enumerate(motif):
+            score = diff(seq, k)
+            if score <= 1:
+                print(f">{fasta_id}\npos:{index+1} {seq}")
